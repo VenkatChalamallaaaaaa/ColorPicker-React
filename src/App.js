0bffbcb7cc4832637574b26c3ac8b2fd07d83f25
Venkat Chalamalla <venkat.chalamallak@gmail.com>
2023-10-14 09:53:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import ColorPicker from './components/ColorPicker';
+
+const colors = ['#fe0000', '#00ff01', '#0000fd', '#feff00', '#ff00fe','#00ffff','#fea500','#81007f','#fcc1cb','#008001','#fe6347','#01ced1','#8b4512','#ff8b00','#4582b4','#fed700'];
 
 function App() {
+  const [backgroundColor, setBackgroundColor] = useState('#FFFFFF'); 
+
+  const handleColorSelect = (color) => {
+    setBackgroundColor(color); 
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='title'>Color Picker</h1>
+      <div className="App" style={{ backgroundColor }}>
+      <ColorPicker colors={colors} backgroundColor={backgroundColor} onColorSelect={handleColorSelect} />
+      </div>
     </div>
   );
 }
